b36462f19de3ce192a45e620a43d1406210b860e
midCenturyDude7 <mgriffes@gmail.com>
2024-10-08 19:50:07 -0700

    Adjusted styles

--- app/components/Footer/Footer.jsx
+++ app/components/Footer/Footer.jsx
@@ -1,3 +1,7 @@
 export default function Footer() {
-  return <div className="p-8">Footer</div>;
+  return (
+    <footer className="border-t-[1px] border-slate-300/20 bg-[#2a3765]/40 shadow-2xl shadow-white">
+      <div className="p-8">Footer</div>
+    </footer>
+  );
 }
--- app/components/Header/Header.jsx
+++ app/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <header className="border-b-[1px] border-slate-300/20 py-8">
+    <header className="border-b-[1px] border-slate-300/10 py-8 shadow-2xl">
       <div className="px-8">Header</div>
     </header>
   );
