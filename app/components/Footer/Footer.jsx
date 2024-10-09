e530c65dc647c1e4627ebb3fbe57c031ef48c790
midCenturyDude7 <mgriffes@gmail.com>
2024-10-08 20:53:43 -0700

    Set up burger menu structure

--- app/components/Footer/Footer.jsx
+++ app/components/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="border-t-[1px] border-slate-300/20 bg-[#2a3765]/40 shadow-2xl shadow-white">
+    <footer className="border-t-[1px] border-slate-300/20 bg-[#2a3765]/30 shadow-2xl shadow-slate-100">
       <div className="p-8">Footer</div>
     </footer>
   );
--- app/components/Header/Header.jsx
+++ app/components/Header/Header.jsx
@@ -1,7 +1,18 @@
+"use client";
+import React from "react";
+import { motion, useCycle, AnimatePresence, MotionConfig } from "framer-motion";
+
 export default function Header() {
   return (
-    <header className="border-b-[1px] border-slate-300/10 py-8 shadow-2xl">
-      <div className="px-8">Header</div>
+    <header className="flex border-b-[1px] border-slate-300/10 py-8 shadow-2xl">
+      <div className="flex-grow px-8">Logo</div>
+      <div className="px-8">
+        <button className="flex flex-col space-y-2">
+          <span className="block h-[0.125rem] w-8 bg-slate-400/75" />
+          <span className="block h-[0.125rem] w-8 bg-slate-400/75" />
+          <span className="block h-[0.125rem] w-8 bg-slate-400/75" />
+        </button>
+      </div>
     </header>
   );
 }
