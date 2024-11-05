d14f1779a052d204783050a76ef4e0678a67b994
midCenturyDude7 <mgriffes@gmail.com>
2024-11-04 20:57:13 -0800

    Preparing Links

--- app/components/Header/MobileNav/MobileNav.jsx
+++ app/components/Header/MobileNav/MobileNav.jsx
@@ -1,11 +1,14 @@
 "use client";
 import React from "react";
-import { motion, useCycle, AnimatePresence, MotionConfig } from "framer-motion";
+import { usePathname } from "next/navigation";
+import { motion, useCycle, AnimatePresence } from "framer-motion";
 import { menuSlide, slide } from "../../../lib/anim";
 import Curve from "./Curve/Curve";
 
 export default function MobileNav() {
+  const pathname = usePathname();
   const [mobileNavbar, toggleMobileNavbar] = useCycle(false, true);
+  const [selectMobileTab, setSelectMobileTab] = React.useState(pathname);
 
   return (
     <nav className="px-8">
@@ -40,67 +43,63 @@ export default function MobileNav() {
       </div>
       <AnimatePresence mode="wait">
         {mobileNavbar && (
-          <MotionConfig
-            transition={{
-              type: "spring",
-              bounce: 0.099,
-            }}
+          <motion.div
+            key="mobile-navbar"
+            variants={menuSlide}
+            initial="initial"
+            animate="enter"
+            exit="exit"
+            className="fixed inset-0 flex h-full flex-col items-center justify-center space-y-10 bg-slate-100 p-6"
           >
-            <motion.div
-              key="mobile-navbar"
-              variants={menuSlide}
-              initial="initial"
-              animate="enter"
-              exit="exit"
-              className="fixed inset-0 flex h-full flex-col items-center justify-center space-y-10 bg-slate-100 p-6"
-            >
-              <div>
-                <motion.ul
-                  variants={slide}
-                  initial="initial"
-                  animate="enter"
-                  exit="exit"
-                  className="space-y-5 text-4xl text-slate-400/80"
-                >
-                  <li>
-                    <a href="#">Link #1</a>
-                  </li>
-                  <li>
-                    <a href="#">Link #2</a>
-                  </li>
-                  <li>
-                    <a href="#">Link #3</a>
-                  </li>
-                </motion.ul>
-              </div>
-              <motion.div className="h-px w-full bg-slate-500/20" />
-              <motion.div
+            <div>
+              <motion.ul
                 variants={slide}
                 initial="initial"
                 animate="enter"
                 exit="exit"
+                className="space-y-5 text-4xl text-slate-400/80"
               >
-                <motion.ul
-                  variants={slide}
-                  initial="initial"
-                  animate="enter"
-                  exit="exit"
-                  className="flex items-center justify-center space-x-8"
-                >
-                  <li>
-                    <div className="h-10 w-10 rounded-lg bg-slate-400/80" />
-                  </li>
-                  <li>
-                    <div className="h-10 w-10 rounded-lg bg-slate-400/80" />
-                  </li>
-                  <li>
-                    <div className="h-10 w-10 rounded-lg bg-slate-400/80" />
-                  </li>
-                </motion.ul>
-              </motion.div>
-              <Curve />
+                <li>
+                  <a href="#">Home</a>
+                </li>
+                <li>
+                  <a href="#">About</a>
+                </li>
+                <li>
+                  <a href="#">Resources</a>
+                </li>
+                <li>
+                  <a href="#">Contact</a>
+                </li>
+              </motion.ul>
+            </div>
+            <motion.div className="h-px w-full bg-slate-500/20" />
+            <motion.div
+              variants={slide}
+              initial="initial"
+              animate="enter"
+              exit="exit"
+            >
+              <motion.ul
+                variants={slide}
+                initial="initial"
+                animate="enter"
+                exit="exit"
+                className="flex items-center justify-center space-x-8"
+              >
+                <li>
+                  <div className="h-10 w-10 rounded-lg bg-slate-400/80" />
+                </li>
+                <li>
+                  <div className="h-10 w-10 rounded-lg bg-slate-400/80" />
+                </li>
+                <li>
+                  <div className="h-10 w-10 rounded-lg bg-slate-400/80" />
+                </li>
+              </motion.ul>
             </motion.div>
-          </MotionConfig>
+            <Curve />
+          </motion.div>
         )}
       </AnimatePresence>
     </nav>
